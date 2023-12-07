8eb472c4103872d39916a6b308758a5915f55402
milldr <miller0daniel@gmail.com>
2023-12-07 11:18:05 -0800

    configs

---
+++ iterm.sh
@@ -0,0 +1,11 @@
+# https://shyr.io/blog/sync-iterm2-configs
+#
+# Specify the preferences directory
+defaults write com.googlecode.iterm2 PrefsCustomFolder -string "~/Documents/dev/setup/config/iterm_settings/"
+
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
+
+# Automatically update
+defaults write com.googlecode.iterm2 SUEnableAutomaticChecks -bool true
+
